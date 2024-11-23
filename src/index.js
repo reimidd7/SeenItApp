888d3e485e728ed845c3599f05e8690f6fb96449
Reilly <125395887+reimidd7@users.noreply.github.com>
2024-11-23 11:50:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
-
-// Accept hot module replacement
-if (module.hot) {
-    module.hot.accept();
-}
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
